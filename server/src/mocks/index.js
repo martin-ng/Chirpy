9ca275e71de87b85e8b23dd4698e96ddf70a14e5
Martin Ng <martinng1992@gmail.com>
2020-01-01 16:55:46 -0500

    added password hashing to db

--- server/src/mocks/index.js
+++ server/src/mocks/index.js
@@ -1,12 +1,14 @@
 import faker from "faker";
 
 import Chirp from "../models/Chirp";
+import User from "../models/User";
 
 const CHIRPS_TOTAL = 10;
 
 export default async () => {
   try {
     await Chirp.deleteMany();
+    await User.remove();
 
     await Array.from({ length: CHIRPS_TOTAL }).forEach(async () => {
       await Chirp.create({ text: faker.lorem.paragraphs(1) });
