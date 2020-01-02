a340e174afbbd8527c73c45895731d6d55c86927
Martin Ng <martinng1992@gmail.com>
2020-01-02 17:41:45 -0500

    fixed sign up auth

--- server/src/config/middleware.js
+++ server/src/config/middleware.js
@@ -1,9 +1,35 @@
 const { ApolloServer, gql } = require("apollo-server-express");
-import typeDefs from "./graphql/schema";
-import resolvers from "./graphql/resolvers";
-import constants from "./config/constants";
+import typeDefs from "../graphql/schema";
+import resolvers from "../graphql/resolvers";
+import { decodeToken } from "../services/auth";
+// import constants from "../config/constants";
+
+async function auth(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    if (token != null) {
+      const user = await decodeToken(token);
+      req.user = user;
+    } else {
+      req.user = null;
+    }
+    return next();
+  } catch (error) {
+    throw Error;
+  }
+}
 
 export default app => {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  app.use(auth);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      user: req.user;
+      // const token = req.headers.authorization || "";
+      // const user = getUser(token);
+      // return { user };
+    }
+  });
   server.applyMiddleware({ app });
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,13 @@
 /* eslint-disable no-console */
 
 import express from "express";
-// const express = require("express");
-// import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
-// const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
-// import { makeExecutableSchema } from "graphql-tools";
-// const { makeExecutableSchema } = require("graphql-tools");
-// import bodyParser from "body-parser";
-// const bodyParser = require("body-parser");
-import { createServer } from "http";
-const { ApolloServer, gql } = require("apollo-server-express");
-
 import "./config/db";
-import constants from "./config/constants";
-import typeDefs from "./graphql/schema";
-import resolvers from "./graphql/resolvers";
 import mocks from "./mocks";
 import middleware from "./config/middleware";
-// import { Mongoose } from "mongoose";
 
 const app = express();
 
 middleware(app);
-
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const PORT = process.env.PORT || 3000;
 
@@ -36,32 +20,3 @@ mocks().then(() => {
     }
   });
 });
-
-// app.listen({ port: PORT }, () =>
-//   console.log(`Server ready at ${PORT} and ${server.graphqlPath}`)
-// );
-// app.use(bodyParser.json());
-
-// app.use(
-//   "/graphiql",
-//   graphqlExpress({
-//     endpointURL: constants.GRAPHQL_PATH
-//   })
-// );
-
-// app.use(
-//   constants.GRAPHQL_PATH,
-//   graphqlExpress({
-//     schema
-//   })
-// );
-
-// const graphQLServer = createServer(app);
-
-// graphQLServer.listen(constants.PORT, err => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(`App listen to port: ${PORT}`);
-//   }
-// });
