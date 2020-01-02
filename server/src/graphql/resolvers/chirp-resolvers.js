6114f04c197a299d7a7ae3657eddcaeb8341cc8b
Martin Ng <martinng1992@gmail.com>
2020-01-02 17:53:22 -0500

    saving commit checkpoint

--- server/src/graphql/resolvers/chirp-resolvers.js
+++ server/src/graphql/resolvers/chirp-resolvers.js
@@ -3,7 +3,13 @@ import Chirp from "../../models/Chirp";
 export default {
   getChirp: (_, { _id }) => Chirp.findById(_id),
   getChirps: () => Chirp.find({}).sort({ createdAt: -1 }),
-  createChirp: (_, args) => Chirp.create(args),
+  createChirp: (_, args, ctx) => {
+    console.log("===============");
+    console.log("CONTEXT", ctx);
+    console.log("===============");
+    return Chirp.create(args);
+  },
+  // createChirp: (_, args) => Chirp.create(args),
   updateChirp: (_, { _id, ...rest }) =>
     Chirp.findByIdAndUpdate(_id, rest, { new: true }),
   deleteChirp: async (_, { _id }) => {
