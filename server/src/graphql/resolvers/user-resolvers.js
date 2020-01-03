1bae43ee3b4f9f696149b61ead3c87d6c3a5e46c
Martin Ng <martinng1992@gmail.com>
2020-01-02 21:37:00 -0500

    committing auth until me

--- server/src/graphql/resolvers/user-resolvers.js
+++ server/src/graphql/resolvers/user-resolvers.js
@@ -1,29 +1,44 @@
 import User from "../../models/User";
+import { reqAuth } from "../../services/auth";
 
 export default {
   signup: async (_, { fullName, ...rest }) => {
     const [firstName, lastName] = fullName.split(" ");
-    const user = await User.create({ firstName, lastName, ...rest });
 
-    return {
-      token: user.createToken()
-    };
-    // return User.create({
-    //   firstName,
-    //   lastName,
-    //   ...rest
-    // });
+    try {
+      const user = await User.create({ firstName, lastName, ...rest });
+      return {
+        token: user.createToken()
+      };
+    } catch (error) {
+      throw error;
+    }
   },
+
   login: async (_, { email, password }) => {
     const user = await User.findOne({ email });
-
-    if (!user) {
-      throw new Error("This user does not exist!");
+    try {
+      if (!user) {
+        throw new Error("This user does not exist!");
+      }
+      if (!user.authUser(password)) {
+        throw new Error("Password does not match!");
+      }
+      return {
+        token: user.createToken()
+      };
+    } catch (error) {
+      throw error;
     }
-    if (!user.authenticateUser(password)) {
-      throw new Error("Password does not match!");
-    }
+  },
 
-    return user;
+  me: async (_, args, { user }) => {
+    try {
+      await reqAuth(user);
+      return User.findById(user._id);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 };
